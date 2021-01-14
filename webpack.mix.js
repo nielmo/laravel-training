3a966b368233decca1a60f3edbf439ff20cc211d
nielmo <roniel.labrador@gmail.com>
2021-01-14 16:01:38 +0800

    Feat: update laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,14 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix
+    .js('resources/js/app.js', 'public/js')
+
+    .postCss('resources/css/app.css', 'public/css')
+
+    .sourceMaps(true, 'source-map')
+    .disableNotifications();
+
+if (mix.inProduction()) {
+    mix.version();
+}
